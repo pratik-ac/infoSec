bca7960fc7471079d91bfa4eed63865993e1ef85
pratik-ac <pra7ik@outlook.com>
2025-01-15 21:13:50 +0545

    feat(security): add helmet middleware to hide Express version

--- myApp.js
+++ myApp.js
@@ -1,58 +1,22 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// * Hackers can exploit known vulnerabilities in Express/Node if they see that your site is powered by Express.
+// * X-Powered-By: Express is sent in every request coming from Express by default.
+// * Use the helmet.hidePoweredBy() middleware
+app.use(
+  helmet.hidePoweredBy({
+    setTo: 'PHP 4.2.0',
+  })
+);
 
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
-app.get("/", function (request, response) {
+app.get('/', function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
